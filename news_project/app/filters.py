394f0951cc68ea710bcda1360d8d5024214cb46f
NaotoWatanabe-NW <jackjazzandrock@gmail.com>
2023-12-16 18:13:25 +0900

    edit django app

--- news_project/app/filters.py
+++ news_project/app/filters.py
@@ -1,6 +1,6 @@
 from django_filters import filters
 from django_filters import FilterSet
-from .models import Item
+from .models import Item, Article
 
 
 class MyOrderingFilter(filters.OrderingFilter):
@@ -10,3 +10,29 @@ class MyOrderingFilter(filters.OrderingFilter):
 class ItemFilter(FilterSet):
     name = filters.CharFilter(label="Name", lookup_expr="contains")
     memo = filters.CharFilter(label="Memo", lookup_expr="contains")
+
+    order_by = MyOrderingFilter(
+        fields=(("name", "name"), ("age", "age")),
+        field_labels={"name": "Name", "age": "Age"},
+        label="Order"
+    )
+
+    class Meta:
+        model = Item
+        fields = ("name", "sex", "memo")
+
+
+class ArticleFilter(FilterSet):
+    title = filters.CharFilter(label="Title", lookup_expr="contains")
+    article = filters.CharFilter(label="Article", lookup_expr="contains")
+
+    order_by = MyOrderingFilter(
+        fields=(("title", "title"), ("article", "article")),
+        field_labels={"title": "Title", "article": "Article"},
+        label="Order"
+    )
+    
+    class Meta:
+
+        model = Article
+        fields = ("title", "article", "url")
